8e09ab96138eebb8865ab676329ef21c4d50b1e7
Arthaud <arthaudp33@gmail.com>
2025-02-15 14:15:02 +0100

    :wrench: Redirect old urls to root

--- astro.config.ts
+++ astro.config.ts
@@ -3,4 +3,25 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://arthaudproust.fr",
+  redirects: {
+    "/galleries": "/",
+    "/galleries/architecture": "/",
+    "/galleries/portraits": "/",
+    "/galleries/street": "/",
+    "/lab": "/",
+    "/references": "/",
+    "/resources": "/",
+    "/readings": "/",
+    "/projects": "/",
+    "/mentions-legales": "/",
+    "/site-map": "/",
+    "/projects/nuit-du-musba": "/",
+    "/projects/life": "/",
+    "/projects/escalade-montesquieu": "/",
+    "/projects/sista": "/",
+    "/projects/too-well-stolen": "/",
+    "/projects/web-dressing": "/",
+    "/projects/preview": "/",
+    "/projects/porte-telephone-en-bois": "/",
+  },
 });
